45d4b05d8ea6e8edf382414d164613fc099bd8e4
VenomImortal <temboarsenio@gmail.com>
2026-01-03 20:02:07 +0100

    Update login.js

--- rotas/login.js
+++ rotas/login.js
@@ -1,27 +1,32 @@
 const express = require("express")
 const login = express.Router()
-const USERNAME = process.env.USERNAME_ADMIN
-const PASSWORD = process.env.SENHA_ADMIN
 
-login.get("/admin/login", (req, res)=>{
-    res.render("login", {ocultarFooter: true})
+login.get("/admin/login", (req, res) => {
+  res.render("login", { ocultarFooter: true })
 })
 
-login.post("/admin/login",(req, res)=>{
-    const {usuario, senha}= req.body
-    if(USERNAME === usuario && PASSWORD===senha){
-        req.session.admin = true
-        console.log("logado com sucesso")
-        res.redirect("/")
+login.post("/admin/login", (req, res) => {
+  const { usuario, senha } = req.body
+
+  if (
+    usuario === process.env.USERNAME_ADMIN &&
+    senha === process.env.SENHA_ADMIN
+  ) {
+    req.session.admin = {
+      usuario: usuario
     }
-    else{
-        res.redirect("/admin/login")
-    }
+
+    console.log("Admin logado com sucesso")
+    return res.redirect("/admin")
+  } else {
+    return res.redirect("/admin/login")
+  }
 })
 
-login.get("/logout", (req, res)=>{
-    req.session.destroy(()=>{
-        res.redirect("/")
-    })
+login.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/")
+  })
 })
+
 module.exports = login
