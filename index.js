62a315b700b0a8efa67707b87a21077accf94788
VenomImortal <temboarsenio@gmail.com>
2026-01-03 16:02:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,52 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const Post = require("./models/post")
 const perfil = require("./models/perfil")
-const {engine: exphbs} = require("express-handlebars")
+const { engine: exphbs } = require("express-handlebars")
 const handlebars = require("handlebars")
 const marked = require("marked")
 const path = require("path")
 const fs = require("fs")
 const fileupload = require("express-fileupload")
 const session = require("express-session")
+const MySQLStore = require("express-mysql-session")(session)
+
+//=============== Configuração do MySQLStore ===============
+const dbUrl = new URL(process.env.DATABASE_URL)
+
+const sessionStore = new MySQLStore({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace('/', ''),
+  port: dbUrl.port || 3306
+})
+
+//=============== Configuração da Session ===============
+app.set('trust proxy', 1) // importante para Render (HTTPS)
 
 app.use(session({
-    secret:"venomimortal",
-    resave:false,
-    saveUninitialized: false,
-    cookie:{maxAge: 60000 * 15}
+  name: 'capitalzero.sid',
+  secret: process.env.SESSION_SECRET || 'venomimortal',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: true,       // HTTPS obrigatório
+    httpOnly: true,
+    sameSite: 'none',   // necessário para Render / cross-site
+    maxAge: 1000 * 60 * 60 * 24 // 1 dia
+  }
 }))
 
+//=============== Variáveis Locais para Handlebars ===============
 app.use((req, res, next) => {
-    
-  res.locals.admin = req.session.admin || false;
-  next();
-});
+  res.locals.admin = req.session.admin || false
+  next()
+})
 
-app.use(express.urlencoded({extended: true}))
+//=============== Body Parser ===============
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 
 //========== Exportação De Rotas ======================
 const inicioRota = require("./rotas/inicio")
@@ -37,7 +59,7 @@ const cadastrarRota = require("./rotas/cadastrarPost")
 const leituraRota = require("./rotas/leituraIndividual")
 const eliminarRota = require("./rotas/eliminarPost")
 const categoriaRota = require("./rotas/categoria")
-const loginRota =require("./rotas/login")
+const loginRota = require("./rotas/login")
 const sobreRota = require("./rotas/sobre")
 const contactoRota = require("./rotas/contacto")
 const feedbackRota = require("./rotas/feedback")
@@ -45,40 +67,33 @@ const removerfeedbackRota = require("./rotas/eliminarFeedback")
 const { helpers } = require("handlebars")
 const { text } = require("body-parser")
 
-//=====================================================
-
-//=============== Liberar Pasta de Arquivo ============
-
+//=============== Liberar Pasta de Arquivo ===============
 app.use(express.static("public"))
 
-
 //============= CONFIG HANDLEBARS =====================
+app.engine('handlebars', exphbs({
+  defaultLayout: "main",
+  partialsDir: path.join(__dirname, "views", "partials"),
+  helpers: {
+    formatarTexto: function (texto) {
+      if (!texto) return ""
 
-app.engine('handlebars', exphbs({defaultLayout: "main", partialsDir: path.join(__dirname, "views", "partials"), helpers:{
-   formatarTexto: function (texto){
-        if(!texto) return "";
-
-        let formatado = texto.replace(/\*\*(.*?)\*\*/g, '<strong>$</strong>');
-
-        formatado = formatado.replace(/\n/g, '<br>');
-
-        formatado = formatado.replace(/ /g, '&nbsp;&nbsp;');
-         return new handlebars.SafeString(formatado);
-   },
-    limitar: function(texto){
-        return texto.length > 15 ? texto.substring(0, 150)+"...": texto
+      let formatado = texto.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+      formatado = formatado.replace(/\n/g, '<br>')
+      formatado = formatado.replace(/ /g, '&nbsp;&nbsp;')
+      return new handlebars.SafeString(formatado)
+    },
+    limitar: function (texto) {
+      return texto.length > 150 ? texto.substring(0, 150) + "..." : texto
     }
-
-}}))
+  }
+}))
 app.set("view engine", 'handlebars')
 //=====================================================
 
-
 app.use(fileupload())
 
-
 //============== USAR ROTAS EXPORTADAS ================
-
 app.use("/", inicioRota)
 app.use("/", categoriaRota)
 app.use("/admin", adminRota)
@@ -91,10 +106,7 @@ app.use("/", sobreRota)
 app.use("/", contactoRota)
 app.use("/admin", removerfeedbackRota)
 
-
-
 //=====================================================
-
-app.listen(PORT, ()=>{
-    console.log(`servidor rodando na porta ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
